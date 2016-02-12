36b9e4d24c1314ed8b2447ddde5b71a69b7c75a5
Eric Kascic <eric.kascic@stelligent.com>
2016-02-12 21:11:00 +0000

    oops need to dereference bucket name

--- src/main/ruby/handler.rb
+++ src/main/ruby/handler.rb
@@ -3,6 +3,6 @@
 require 'aws-sdk'
 s3 = Aws::S3::Client.new
 list_buckets_response = s3.list_buckets
-list_buckets_response.buckets.each do |bucket_name|
-  $lambdaLogger.log(bucket_name)
+list_buckets_response.buckets.each do |bucket|
+  $lambdaLogger.log(bucket[:name])
 end
