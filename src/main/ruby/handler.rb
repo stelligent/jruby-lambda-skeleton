eb5308545d385cc6bc995f5b9293bcc1f7804fa7
Eric Kascic <eric.kascic@stelligent.com>
2016-02-13 01:11:50 +0000

    pull in rspec

--- src/main/ruby/handler.rb
+++ src/main/ruby/handler.rb
@@ -6,3 +6,7 @@ list_buckets_response = s3.list_buckets
 list_buckets_response.buckets.each do |bucket|
   $lambdaLogger.log(bucket[:name])
 end
+
+$lambdaLogger.log(Dir.entries('.').to_s)
+require 'rspec/core'
+RSpec::Core::Runner.run ['spec']
---
+++ src/main/ruby/spec/test_me_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+
+describe 'a beautiful day at Downton Abbey' do
+
+  context "when Matthew decides to pay attention to Downton's finances" do
+
+    it 'sends his precocious wife into apoplexy' do
+      fail 'Matthew should tread lightly!'
+    end
+  end
+end
